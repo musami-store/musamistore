26423d12d8c57d67651148b4434977db23e2d97b
musami-store <musami.store@gmail.com>
2025-12-17 23:59:04 -0300

    Add files via upload

--- products.js
+++ products.js
@@ -19,4 +19,94 @@ const PRODUCTS = {
         price: 1099,
         images: ["Products/Pendientes con estrella.png"]
     },
-};
+    "aros-argolla-punta-gruesa":{
+        title: 'Aros',
+        variants: {
+            "plateado": {
+                label: "Plateado",
+                price: 1500,
+                images: ["Products/aros-argolla-punta-gruesa-1.jpg", "Products/aros-argolla-punta-gruesa-2.jpg", "Products/aros-argolla-punta-gruesa-3.jpg"],
+            },
+            "dorado": {
+                label: "Dorado - AGOTADO",
+                price: 1500,
+                images: ["Products/aros-argolla-punta-gruesa-3.jpg", "Products/aros-argolla-punta-gruesa-1.jpg", "Products/aros-argolla-punta-gruesa-2.jpg"],
+            }
+        }
+    },
+    "aros-colgante-circulo-cadena":{
+        title: 'Aros',
+        variants: {
+            "plateado": {
+                label: "Plateado",
+                price: 1500,
+                images: ["Products/aros-colgante-circulo-cadena-2.jpg", "Products/aros-colgante-circulo-cadena-1.jpg", "Products/aros-colgante-circulo-cadena-3.jpg", "Products/aros-colgante-circulo-cadena-4.jpg"],
+            },
+            "dorado": {
+                label: "Dorado - AGOTADO",
+                price: 1500,
+                images: ["Products/aros-colgante-circulo-cadena-1.jpg", "Products/aros-colgante-circulo-cadena-2.jpg", "Products/aros-colgante-circulo-cadena-3.jpg", "Products/aros-colgante-circulo-cadena-4.jpg"],
+            }
+        }
+    },
+    "aros-argolla-circulo-corazon":{
+        title: 'Argollas',
+        variants: {
+            "dorado": {
+                label: "Dorado",
+                price: 1500,
+                images: ["Products/aros-argolla-circulo-corazon-2.jpg", "Products/aros-argolla-circulo-corazon-1.jpg", "Products/aros-argolla-circulo-corazon-3.jpg", "Products/aros-argolla-circulo-corazon-4.jpg"],
+            },
+            "plateado": {
+                label: "Plateado - AGOTADO",
+                price: 1500,
+                images: ["Products/aros-argolla-circulo-corazon-1.jpg", "Products/aros-argolla-circulo-corazon-2.jpg", "Products/aros-argolla-circulo-corazon-3.jpg", "Products/aros-argolla-circulo-corazon-4.jpg"],
+            },
+        }
+    },
+    "aros-limon":{
+        title: 'Aros',
+        variants: {
+            "rosado":{
+                label: "Rosado",
+                price: 2000,
+                images: ["Products/aros-limon-1.jpg", "Products/aros-limon-2.jpg"]
+            },
+            "amarillo":{
+                label: "Amarillo - AGOTADO",
+                price: 2000,
+                images: ["Products/aros-limon-1.jpg", "Products/aros-limon-3.jpg"]
+            }
+        }
+    },
+    "aros-estrella-perlas":{
+        title: 'Aros',
+        price: 1600,
+        images: ["Products/aros-estrella-perlas-1.jpg", "Products/aros-estrella-perlas-2.jpg", "Products/aros-estrella-perlas-3.jpg"]
+    },
+    "argollas-plateada-espiral":{
+        title: 'Argollas',
+        price: 1500,
+        images: ["Products/argollas-plateada-espiral-1.jpg", "Products/argollas-plateada-espiral-2.jpg", "Products/argollas-plateada-espiral-3.jpg"]
+    },
+    "aros-gota-naranja-rombo":{
+        title: 'Aros',
+        price: 1800,
+        images: ["Products/aros-gota-naranja-rombo-1.jpg", "Products/aros-gota-naranja-rombo-2.jpg", "Products/aros-gota-naranja-rombo-3.jpg", "Products/aros-gota-naranja-rombo-4.jpg"]  
+    },
+    "aros-flor-hojas-mantel":{
+        title: 'Aros',
+        variants: {
+            "negro":{
+                label: "Negro",
+                price: 1800,
+                images: ["Products/aros-flor-hojas-mantel-1.jpg", "Products/aros-flor-hojas-mantel-2.jpg", "Products/aros-flor-hojas-mantel-3.jpg", "Products/aros-flor-hojas-mantel-4.jpg"]
+            },
+            "rosado":{
+                label: "Rosado - AGOTADO",
+                price: 1800,
+                images: ["Products/aros-flor-hojas-mantel-2.jpg", "Products/aros-flor-hojas-mantel-1.jpg", "Products/aros-flor-hojas-mantel-3.jpg", "Products/aros-flor-hojas-mantel-4.jpg"]
+            }
+        }
+    }
+}
