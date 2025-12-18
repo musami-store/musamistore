1eccbf9fbcbe01223c810a23b2b1601f6e6f64c5
musami-store <musami.store@gmail.com>
2025-12-18 15:57:40 -0300

    Add files via upload

--- products.js
+++ products.js
@@ -97,15 +97,15 @@ const PRODUCTS = {
     "aros-flor-hojas-mantel":{
         title: 'Aros',
         variants: {
-            "negro":{
-                label: "Negro",
-                price: 1800,
-                images: ["Products/aros-flor-hojas-mantel-1.jpg", "Products/aros-flor-hojas-mantel-2.jpg", "Products/aros-flor-hojas-mantel-3.jpg", "Products/aros-flor-hojas-mantel-4.jpg"]
-            },
             "rosado":{
-                label: "Rosado - AGOTADO",
+                label: "Rosado",
                 price: 1800,
                 images: ["Products/aros-flor-hojas-mantel-2.jpg", "Products/aros-flor-hojas-mantel-1.jpg", "Products/aros-flor-hojas-mantel-3.jpg", "Products/aros-flor-hojas-mantel-4.jpg"]
+            },
+            "negro":{
+                label: "Negro - AGOTADO",
+                price: 1800,
+                images: ["Products/aros-flor-hojas-mantel-1.jpg", "Products/aros-flor-hojas-mantel-2.jpg", "Products/aros-flor-hojas-mantel-3.jpg", "Products/aros-flor-hojas-mantel-4.jpg"]
             }
         }
     },
@@ -178,5 +178,416 @@ const PRODUCTS = {
         title: 'Argollas',
         price: 1000,
         images: ["Products/argollas-corazones-1.jpg", "Products/argollas-corazones-2.jpg", "Products/argollas-corazones-3.jpg", "Products/argollas-corazones-4.jpg"]
+    },
+    "argollas-perlas-pequeñas":{
+        title: 'Argollas',
+        variants: {
+            "dorado":{
+                label: "Dorado",
+                price: 1200,
+                images: [
+                    "Products/argollas-perlas-pequeñas-1.jpg", "Products/argollas-perlas-pequeñas-2.jpg",
+                    "Products/argollas-perlas-pequeñas-3.jpg", "Products/argollas-perlas-pequeñas-4.jpg",
+                    "Products/argollas-perlas-pequeñas-5.jpg", "Products/argollas-perlas-pequeñas-6.jpg",
+                    "Products/argollas-perlas-pequeñas-7.jpg", "Products/argollas-perlas-pequeñas-8.jpg",
+                    "Products/argollas-perlas-pequeñas-9.jpg", "Products/argollas-perlas-pequeñas-10.jpg"
+                ]
+            },
+            "plateado":{
+                label: "Plateado - AGOTADO",
+                price: 1200,
+                images: [
+                    "Products/argollas-perlas-pequeñas-1.jpg", "Products/argollas-perlas-pequeñas-2.jpg",
+                    "Products/argollas-perlas-pequeñas-3.jpg", "Products/argollas-perlas-pequeñas-4.jpg",
+                    "Products/argollas-perlas-pequeñas-5.jpg", "Products/argollas-perlas-pequeñas-6.jpg",
+                    "Products/argollas-perlas-pequeñas-7.jpg", "Products/argollas-perlas-pequeñas-8.jpg",
+                    "Products/argollas-perlas-pequeñas-9.jpg", "Products/argollas-perlas-pequeñas-10.jpg"
+                ]
+            },
+        }  
+    },
+    "argollas-diamante-corazon":{
+        title: 'Argollas',
+        price: 1500,
+        images: [
+            "Products/argollas-diamante-corazon-1.jpg", "Products/argollas-diamante-corazon-2.jpg", 
+            "Products/argollas-diamante-corazon-3.jpg", "Products/argollas-diamante-corazon-4.jpg", "Products/argollas-diamante-corazon-5.jpg"
+        ]
+    },
+    "argollas-espiral-grande":{
+        title: 'Argollas',
+        variants: {
+            "dorado":{
+                label: "Dorado",
+                price: 800,
+                images: [
+                    "Products/argollas-espiral-grande-1.jpg", "Products/argollas-espiral-grande-2.jpg",
+                    "Products/argollas-espiral-grande-3.jpg", "Products/argollas-espiral-grande-4.jpg",
+                    "Products/argollas-espiral-grande-5.jpg", "Products/argollas-espiral-grande-6.jpg"
+                ]
+            },
+            "plateado":{
+                label: "Plateado",
+                price: 800,
+                images: [
+                    "Products/argollas-espiral-grande-1.jpg", "Products/argollas-espiral-grande-3.jpg",
+                    "Products/argollas-espiral-grande-2.jpg", "Products/argollas-espiral-grande-4.jpg",
+                    "Products/argollas-espiral-grande-5.jpg", "Products/argollas-espiral-grande-6.jpg"
+                ]
+            },
+        }  
+    },
+    "argollas-trenza-dorado":{
+        title: 'Argollas',
+        price: 1000,
+        images: [
+            "Products/argollas-trenza-dorado-1.jpg", "Products/argollas-trenza-dorado-2.jpg",
+            "Products/argollas-trenza-dorado-3.jpg", "Products/argollas-trenza-dorado-4.jpg",
+            "Products/argollas-trenza-dorado-5.jpg"
+        ]
+    },
+    "aros-lana": {
+        title: 'Aros',
+        variants: {
+            "rosado":{
+                label: "Rosado",
+                price: 1500,
+                images: [
+                    "Products/aros-lana-1.jpg", "Products/aros-lana-2.jpg",
+                    "Products/aros-lana-3.jpg", "Products/aros-lana-4.jpg"
+                ]
+            },
+            "negro":{
+                label: "Negro",
+                price: 1500,
+                images: [
+                    "Products/aros-lana-2.jpg", "Products/aros-lana-1.jpg",
+                    "Products/aros-lana-3.jpg", "Products/aros-lana-4.jpg"
+                ]
+            },
+            "amarillo":{
+                label: "Amarillo",
+                price: 1500,
+                images: [
+                    "Products/aros-lana-3.jpg", "Products/aros-lana-2.jpg",
+                    "Products/aros-lana-1.jpg", "Products/aros-lana-4.jpg"
+                ]
+            },
+            "Rojo":{
+                label: "Rojo",
+                price: 1500,
+                images: [
+                    "Products/aros-lana-4.jpg", "Products/aros-lana-2.jpg",
+                    "Products/aros-lana-1.jpg", "Products/aros-lana-4.jpg"
+                ]
+            },
+        }
+    },
+    "aros-lana-fila": {
+        title: 'Aros',
+        variants: {
+            "rosado":{
+                label: "Rosado",
+                price: 1500,
+                images: [
+                    "Products/aros-lana-fila-1.jpg", "Products/aros-lana-fila-2.jpg",
+                    "Products/aros-lana-fila-3.jpg", "Products/aros-lana-fila-4.jpg",
+                    "Products/aros-lana-fila-5.jpg"
+                ]
+            },
+            "amarillo":{
+                label: "Amarillo",
+                price: 1500,
+                images: [
+                    "Products/aros-lana-fila-1.jpg", "Products/aros-lana-fila-3.jpg",
+                    "Products/aros-lana-fila-2.jpg", "Products/aros-lana-fila-4.jpg",
+                    "Products/aros-lana-fila-5.jpg"
+                ]
+            },
+            "blanco-crema":{
+                label: "Blanco Crema",
+                price: 1500,
+                images: [
+                    "Products/aros-lana-fila-1.jpg", "Products/aros-lana-fila-5.jpg",
+                    "Products/aros-lana-fila-3.jpg", "Products/aros-lana-fila-4.jpg",
+                    "Products/aros-lana-fila-2.jpg"
+                ]
+            },
+            "blanco":{
+                label: "Blanco",
+                price: 1500,
+                images: [
+                    "Products/aros-lana-fila-1.jpg", "Products/aros-lana-fila-4.jpg",
+                    "Products/aros-lana-fila-3.jpg", "Products/aros-lana-fila-2.jpg",
+                    "Products/aros-lana-fila-5.jpg"
+                ]
+            },
+        }
+    },
+    "aros-circulo-lana": {
+        title: 'Aros',
+        variants: {
+            "colores":{
+                label: "Diseño Colores",
+                price: 1200,
+                images: [
+                    "Products/aros-circulo-lana-1.jpg", "Products/aros-circulo-lana-2.jpg",
+                    "Products/aros-circulo-lana-3.jpg", "Products/aros-circulo-lana-4.jpg"
+                ]
+            },
+            "azul":{
+                label: "Azul - AGOTADO",
+                price: 1200,
+                images: [
+                    "Products/aros-circulo-lana-1.jpg", "Products/aros-circulo-lana-3.jpg",
+                    "Products/aros-circulo-lana-2.jpg", "Products/aros-circulo-lana-4.jpg"
+                ]
+            },
+            "negro":{
+                label: "Negro - AGOTADO",
+                price: 1200,
+                images: [
+                    "Products/aros-circulo-lana-1.jpg", "Products/aros-circulo-lana-4.jpg",
+                    "Products/aros-circulo-lana-3.jpg", "Products/aros-circulo-lana-2.jpg"
+                ]
+            }
+        }
+    },
+    "aros-circulo-lana-v4":{
+        title: 'Aros',
+        price: 1200,
+        images: ["Products/aros-circulo-lana-v4.jpg"]
+    },
+    "aros-lana-azul":{
+        title: 'Aros',
+        price: 1500,
+        images: ["Products/aros-lana-azul.jpg"]
+    },
+    "aros-plastico-colores":{
+        title: 'Aros',
+        variants:{
+            "diseño-2": {
+                label:"Diseño 2",
+                price: 1300,
+                images: [
+                    "Products/aros-plastico-colores-all.jpg", "Products/aros-plastico-colores-1.jpg",
+                    "Products/aros-plastico-colores-2.jpg", "Products/aros-plastico-colores-3.jpg",
+                    "Products/aros-plastico-colores-4.jpg", "Products/aros-plastico-colores-5.jpg",
+                    "Products/aros-plastico-colores-6.jpg", "Products/aros-plastico-colores-all2.jpg"
+                ]
+            },
+            "diseño-3": {
+                label:"Diseño 3",
+                price: 1300,
+                images: [
+                    "Products/aros-plastico-colores-all.jpg", "Products/aros-plastico-colores-1.jpg",
+                    "Products/aros-plastico-colores-2.jpg", "Products/aros-plastico-colores-3.jpg",
+                    "Products/aros-plastico-colores-4.jpg", "Products/aros-plastico-colores-5.jpg",
+                    "Products/aros-plastico-colores-6.jpg", "Products/aros-plastico-colores-all2.jpg"
+                ]
+            },
+            "diseño-1": {
+                label:"Diseño 1 - AGOTADO",
+                price: 1300,
+                images: [
+                    "Products/aros-plastico-colores-all.jpg", "Products/aros-plastico-colores-1.jpg",
+                    "Products/aros-plastico-colores-2.jpg", "Products/aros-plastico-colores-3.jpg",
+                    "Products/aros-plastico-colores-4.jpg", "Products/aros-plastico-colores-5.jpg",
+                    "Products/aros-plastico-colores-6.jpg", "Products/aros-plastico-colores-all2.jpg"
+                ]
+            },
+            "diseño-4": {
+                label:"Diseño 4 - AGOTADO",
+                price: 1300,
+                images: [
+                    "Products/aros-plastico-colores-all.jpg", "Products/aros-plastico-colores-1.jpg",
+                    "Products/aros-plastico-colores-2.jpg", "Products/aros-plastico-colores-3.jpg",
+                    "Products/aros-plastico-colores-4.jpg", "Products/aros-plastico-colores-5.jpg",
+                    "Products/aros-plastico-colores-6.jpg", "Products/aros-plastico-colores-all2.jpg"
+                ]
+            },
+            "diseño-5": {
+                label:"Diseño 5 - AGOTADO",
+                price: 1300,
+                images: [
+                    "Products/aros-plastico-colores-all.jpg", "Products/aros-plastico-colores-1.jpg",
+                    "Products/aros-plastico-colores-2.jpg", "Products/aros-plastico-colores-3.jpg",
+                    "Products/aros-plastico-colores-4.jpg", "Products/aros-plastico-colores-5.jpg",
+                    "Products/aros-plastico-colores-6.jpg", "Products/aros-plastico-colores-all2.jpg"
+                ]
+            }
+        }
+    },
+    "aros-diamante-marmol":{
+        title: 'Aros',
+        variants: {
+            "diseño-1":{
+                label: "Diseño 1",
+                price: 1500,
+                images: [
+                    "Products/aros-diamante-marmol-1.jpg", "Products/aros-diamante-marmol-2.jpg"
+                ]
+            },
+            "diseño-2":{
+                label: "Diseño 2 - AGOTADO",
+                price: 1500,
+                images: [
+                    "Products/aros-diamante-marmol-2.jpg", "Products/aros-diamante-marmol-1.jpg"
+                ]
+            }
+        }
+    },
+    "aros-plastico-blanco":{
+        title: 'Aros',
+        price: 1300,
+        images: ["Products/aros-plastico-blanco-1.jpg", "Products/aros-plastico-blanco-2.jpg"]
+    },
+    "aros-limon-v2": {
+        title: 'Aros',
+        price: 1500,
+        images: [
+            "Products/aros-limon-v2-1.jpg", "Products/aros-limon-v2-2.jpg", "Products/aros-limon-v2-3.jpg"
+        ]
+    },
+    "aros-animal-print-leopardo":{
+        title: 'Aros',
+        price: 1800,
+        images:["Products/aros-animal-print-leopardo-1.jpg", "Products/aros-animal-print-leopardo-2.jpg"]
+    },
+    "aros-plastico-morado":{
+        title: 'Argollas',
+        price: 1500,
+        images: [
+            "Products/aros-plastico-morado-1.jpg", "Products/aros-plastico-morado-2.jpg",
+            "Products/aros-plastico-morado-3.jpg", "Products/aros-plastico-morado-4.jpg",
+            "Products/aros-plastico-morado-5.jpg", "Products/aros-plastico-morado-6.jpg"
+        ]
+    },
+    "argollas-plastico-verde-rosa":{
+        title: 'Argollas',
+        variants:{
+            "rosado":{
+                label: "Rosado",
+                price: 1500,
+                images: [
+                    "Products/argollas-plastico-verde-rosa-1.jpg", "Products/argollas-plastico-verde-rosa-2.jpg",
+                    "Products/argollas-plastico-verde-rosa-3.jpg", "Products/argollas-plastico-verde-rosa-4.jpg",
+                    "Products/argollas-plastico-verde-rosa-5.jpg", "Products/argollas-plastico-verde-rosa-6.jpg",
+                    "Products/argollas-plastico-verde-rosa-7.jpg", "Products/argollas-plastico-verde-rosa-8.jpg",
+                    "Products/argollas-plastico-verde-rosa-9.jpg"
+                ]
+            },
+            "verde":{
+                label: "Verde",
+                price: 1500,
+                images: [
+                    "Products/argollas-plastico-verde-rosa-1.jpg", "Products/argollas-plastico-verde-rosa-5.jpg",
+                    "Products/argollas-plastico-verde-rosa-6.jpg", "Products/argollas-plastico-verde-rosa-7.jpg",
+                    "Products/argollas-plastico-verde-rosa-2.jpg", "Products/argollas-plastico-verde-rosa-3.jpg",
+                    "Products/argollas-plastico-verde-rosa-4.jpg", "Products/argollas-plastico-verde-rosa-8.jpg",
+                    "Products/argollas-plastico-verde-rosa-9.jpg"
+                ]
+            }
+        }
+    },
+    "aros-verde-grueso": {
+        title: 'Aros',
+        price: 1300,
+        images: [
+            "Products/aros-verde-grueso-1.jpg", "Products/aros-verde-grueso-2.jpg",
+            "Products/aros-verde-grueso-3.jpg", "Products/aros-verde-grueso-4.jpg",
+            "Products/aros-verde-grueso-5.jpg", "Products/aros-verde-grueso-6.jpg",
+            "Products/aros-verde-grueso-7.jpg"
+        ]
+    },
+    "aros-plastico-tonos-rosa": {
+        title: 'Aros',
+        variants: {
+            "diseño-1":{
+                label: "Diseño 1",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-rosa-1.jpg", "Products/aros-plastico-tonos-rosa-2.jpg"]
+            },
+            "diseño-2":{
+                label: "Diseño 2",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-rosa-1.jpg", "Products/aros-plastico-tonos-rosa-2.jpg"]
+            },
+            "diseño-3":{
+                label: "Diseño 3",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-rosa-1.jpg", "Products/aros-plastico-tonos-rosa-2.jpg"]
+            },
+            "diseño-5":{
+                label: "Diseño 5",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-rosa-1.jpg", "Products/aros-plastico-tonos-rosa-2.jpg"]
+            },
+            "diseño-4":{
+                label: "Diseño 4 - AGOTADO",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-rosa-1.jpg", "Products/aros-plastico-tonos-rosa-2.jpg"]
+            },
+        }
+    },
+    "aros-plastico-tonos-cafe": {
+        title: 'Aros',
+        variants: {
+            "diseño-1":{
+                label: "Diseño 1",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-cafe-1.jpg", "Products/aros-plastico-tonos-cafe-2.jpg"]
+            },
+            "diseño-2":{
+                label: "Diseño 2",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-cafe-1.jpg", "Products/aros-plastico-tonos-cafe-2.jpg"]
+            },
+            "diseño-3":{
+                label: "Diseño 3",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-cafe-1.jpg", "Products/aros-plastico-tonos-cafe-2.jpg"]
+            },
+            "diseño-5":{
+                label: "Diseño 5",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-cafe-1.jpg", "Products/aros-plastico-tonos-cafe-2.jpg"]
+            },
+            "diseño-4":{
+                label: "Diseño 4 - AGOTADO",
+                price: 1300,
+                images: ["Products/aros-plastico-tonos-cafe-1.jpg", "Products/aros-plastico-tonos-cafe-2.jpg"]
+            },
+        }
+    },
+    "argollas-corazon-colores": {
+        title: 'Argollas',
+        variants: {
+            "rosado":{
+                label: "Rosado",
+                price: 1500,
+                images: ["Products/argollas-corazon-colores-1.jpg", "Products/argollas-corazon-colores-2.jpg"]
+            },
+            "verde":{
+                label: "Verde",
+                price: 1500,
+                images: ["Products/argollas-corazon-colores-1.jpg", "Products/argollas-corazon-colores-2.jpg"]
+            },
+            "amarillo":{
+                label: "Amarillo",
+                price: 1500,
+                images: ["Products/argollas-corazon-colores-1.jpg", "Products/argollas-corazon-colores-2.jpg"]
+            },
+            "azul":{
+                label: "Azul",
+                price: 1500,
+                images: ["Products/argollas-corazon-colores-1.jpg", "Products/argollas-corazon-colores-2.jpg"]
+            },
+            "morado":{
+                label: "Morado - AGOTADO",
+                price: 1500,
+                images: ["Products/argollas-corazon-colores-1.jpg", "Products/argollas-corazon-colores-2.jpg"]
+            },
+        }
     }
 }
